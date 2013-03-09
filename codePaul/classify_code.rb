8115f02412c6d443736f6a6bf4d9d9db72dc0041
Mansour Hamidi <mansourhamidi@hotmail.com>
2013-03-09 11:48:30 +0100

    classify_code_test done

--- codePaul/classify_code.rb
+++ codePaul/classify_code.rb
@@ -1,11 +1,11 @@
 
-
+# classifies the code from user input to icd or chop or unknown
+# accepts only exact matches and is case insensitive
 def get_code_type(input)
-  if input.match(/\d{2}\.\w{0,2}(\.\w{0,2})?/)
-    return :chop
-  elsif input.match(/.\d{2}\.\d{1,2}/)
+  if input.match(/^.\d{2}(\.\d{1,2})?$/)
     return :icd
+  elsif input.match(/^\d{2}\.\w{0,2}(\.\w{0,2})?$/)
+    return :chop
   else return :unknown
   end
-
 end
--- codePaul/classify_code_test.rb
+++ codePaul/classify_code_test.rb
@@ -1,16 +1,48 @@
 require 'test/unit'
+require 'minitest/reporters'
 require_relative 'classify_code'
+
+MiniTest::Reporters.use!
 
 class ClassifyCodeTest < Test::Unit::TestCase
 
-
   def setup
-    @input = 'a00.80.00'
+    @chop1 = '00.4D'
+    @chop2 = '89.d3.5C'
+    @chop3 = '99.B6.11'
+
+    @icd1 = 'a66.0'
+    @icd2 = 'K58'
+    @icd3 = 'Z09.22'
+
+    @unknown1 = 'A'
+    @unknown2 = 'c0.4d'
+    @unknown3 = 'z45.P'
+    @unknown4 = 'g3.66.66'
+    @unknown5 = '999.b6.11'
+    @unknown6 = 'ss5.22'
   end
 
 
+  def test_chop
+    assert(get_code_type(@chop1) == :chop)
+    assert(get_code_type(@chop2) == :chop)
+    assert(get_code_type(@chop3) == :chop)
+  end
+
   def test_icd
-    assert(get_code_type(@input) == :chop)
+    assert(get_code_type(@icd1) == :icd)
+    assert(get_code_type(@icd2) == :icd)
+    assert(get_code_type(@icd3) == :icd)
+  end
+
+  def test_unknown
+    assert(get_code_type(@unknown1) == :unknown)
+    assert(get_code_type(@unknown2) == :unknown)
+    assert(get_code_type(@unknown3) == :unknown)
+    assert(get_code_type(@unknown4) == :unknown)
+    assert(get_code_type(@unknown5) == :unknown)
+    assert(get_code_type(@unknown6) == :unknown)
   end
 
 end
